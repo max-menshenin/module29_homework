b47efdd6ac92e7d7d9c84488895dc34a18c9ffcf
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-05-16 15:43:10 +0300

    init

--- Application/Views/contacts_view.php
+++ Application/Views/contacts_view.php
@@ -7,6 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    <p>Hi</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque a repellat ex soluta molestias at vero minima corrupti molestiae suscipit tenetur voluptate dolor, ab ullam sint quisquam culpa in quasi.</p>
 </body>
 </html>
