5ad06a81fc166c6212e28c6e5903ab2df8005e6b
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-05-31 15:41:22 +0300

    Ok

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ require_once __DIR__ . '/bootstrap.php';
       rel="stylesheet"
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
       crossorigin="anonymous">
-      <link rel="stylesheet" href="style.css">
+      <link rel="stylesheet" href="styles/style.css">
   </head>
   <body>
     <ul>
