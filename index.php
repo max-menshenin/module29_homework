7f35b0674b199a065ade9807033b1cb29511f7f0
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-06-01 16:24:49 +0300

    proceed

--- index.php
+++ index.php
@@ -4,27 +4,25 @@ require_once __DIR__ . '/bootstrap.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <title>JS-Start</title>
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-      crossorigin="anonymous">
-      <link rel="stylesheet" href="styles/style.css">
-  </head>
-  <body>
-    <ul>
-      <li>
-        <a href="index.php?url=Main">Index</a>
-      </li>
-      <li>
-        <a href="index.php?url=About">About</a>
-      </li>
-      <li>
-        <a href="index.php?url=Contacts">Contacts</a>
-      </li>
-    </ul>
-  </body>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Main</title>
+  <link rel="stylesheet" href="styles/style.css">
+</head>
+
+<body>
+  <ul>
+    <li>
+      <a href="index.php?url=Main">Index</a>
+    </li>
+    <li>
+      <a href="index.php?url=About">About</a>
+    </li>
+    <li>
+      <a href="index.php?url=Contacts">Contacts</a>
+    </li>
+  </ul>
+</body>
+
 </html>
