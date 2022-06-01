1e41295c197d195b867562a0f42ab6b83fd960a0
Max Menshenin <max.menshenin.dev@yandex.ru>
2022-06-01 20:53:54 +0300

    done

--- Application/Controllers/controlle_about.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class controller_about extends Controller
-{
-    function action_about()
-    {
-        $this->view->generate('about_view.php', 'template_view.php');
-    }
-}
--- Application/Controllers/controller_about.php
+++ Application/Controllers/controller_about.php
@@ -1,7 +1,8 @@
 <?php
 class Controller_about extends Controller {
-    function action_about() {
+    function action_index() {
     $this->view->generate('about_view.php', 'template_view.php');
     }
     
 }
+
